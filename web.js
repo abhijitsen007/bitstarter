e165460f01b697ec3e4a4399b0963a16e369f2f9
Abhijit Sen <abhijit.sen@yahoo.com>
2013-07-13 02:33:47 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,10 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(output);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var buffer = fs.readFileSync(index.html);
+var output = buffer.toString(0, buffer.length);
